bfc7411b0aeba438c805acbe1c30546fd4f962c7
Богдан Суров <bogdansurov@MacBook-Air-Bogdan.local>
2025-07-22 14:01:57 +0300

    Fixed env

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,11 +1,12 @@
-const TOKEN = process.env.API_TOKEN!
+const TOKEN = import.meta.env.VITE_API_TOKEN
 import type { ReactNode } from 'react'
 import { useState } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-	const [token, setToken] = useState<string | null>(TOKEN)
+	const [token, setToken] = useState<string | null>(TOKEN!)
 	// FIXME: изменить на вытаскивание токена из локал сторейдж
+	console.log(TOKEN)
 
 	return (
 		<AuthContext.Provider value={{ token, setToken }}>
